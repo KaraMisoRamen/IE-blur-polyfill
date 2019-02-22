39d7072328616c9b3370e859fae0f05b435a0782
樊沛 <fanpei920@gmail.com>
2019-02-23 00:49:37 +0900

    added rollup config

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'umd'
+    },
+    {
+      file: 'dist/index.es.js',
+      format: 'es'
+    },
+    {
+      file: 'dist/index.amd.js',
+      format: 'amd'
+    },
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs'
+    }
+  ]
+}
